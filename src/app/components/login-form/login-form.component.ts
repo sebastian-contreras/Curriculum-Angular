c39681b9c4cffe44b40ac238dbb74a12e6ae832a
Sebastian Contreras <sebasfac2017@gmail.com>
2023-05-01 13:44:38 -0300

    edicion login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Credentials } from 'src/app/interfaces';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -11,7 +14,16 @@ export class LoginFormComponent {
     email: new FormControl(),
     password: new FormControl()
   })
+  creds: Credentials = {
+    email:'',
+    password:''
+  }
+  constructor(private authService: AuthService,private router: Router){}
   onClick(){
     console.log(this.form.value)
+    this.authService.login(this.creds)
+    .subscribe(response=>{
+      this.router.navigate(['/']);
+    })
   }
 }
